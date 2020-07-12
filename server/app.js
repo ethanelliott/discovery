7304d91e253cd0633bea71266d50f13b984d11cd
ethan <ethan.elliott@hotmail.ca>
2020-07-12 19:43:33 -0400

    cool

--- server/app.js
+++ server/app.js
@@ -1,65 +1,107 @@
+const PORT = 8888;
 const db = require('diskdb');
 const axios = require('axios');
 const app = require('express')();
 const bodyParser = require('body-parser')
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const TABLES = ['connections', 'services'].map(e => e.toUpperCase());
+const TABLES = ['services'].map(e => e.toUpperCase());
 db.connect('./', TABLES);
-const PORT = 8888;
-
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.json({root: '/'});
-});
+const TYPES = {
+    IP_ADDRESS: {
+        regex: new RegExp('^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\\.(?!$)|$)){4}$', 'g'),
+        matches: []
+    },
+    PHONE_NUMBER: {
+        regex: new RegExp('^(\\+\\d{1,2}\\s?)?1?\\-?\\.?\\s?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$', 'g'),
+        matches: []
+    }
+};
 
-app.get('/connections', (req, res) => {
-    const conn = db.CONNECTIONS.find();
-    res.json(conn);
-});
+const ROUTES = [
+    {
+        route: '/',
+        method: 'get',
+        action: (req, res) => {
+            return res.json({
+                name: 'Discovery Service',
+                version: '0.0.1',
+                description: 'Central repository for operating services, including tests to ensure services are running.'
+            });
+        }
+    },
+    {
+        route: '/services',
+        method: 'get',
+        action: (req, res) => {
+            return res.json(db.SERVICES.find());
+        }
+    },
+    {
+        route: '/match',
+        method: 'get',
+        urlParams: ['q'],
+        action: (req, res) => {
+            const q = req.query.q;
+            const results = Object.keys(TYPES)
+                .map(type => q.match(TYPES[type].regex) ? type : null)
+                .filter(t => t !== null)
+                .map(type => ({
+                    type,
+                    match_services: TYPES[type].matches.map(e => {
+                        e.route = e.route.replace('%s', q);
+                        return e;
+                    })
+                }));
+            return res.json({q, results});
+        }
+    },
+    {
+        route: '/register',
+        method: 'post',
+        bodyParams: [],
+        action: async (req, res) => {
+            const info = req.body;
+            const {data} = await axios.get(`${info.address}/test`);
+            db.SERVICES.save(info);
+            info.matcher.forEach(match => {
+                if (TYPES[match.type]) {
+                    TYPES[match.type].matches.push({
+                        id: info._id,
+                        route: `${info.address}${match.route}`
+                    });
+                } else {
+                    console.log(`unknown type: ${match.type}`);
+                }
+            });
+            console.log(`${info.name} has been registered!`);
+            return res.json({success: data.success});
 
-app.get('/services', (req, res) => {
-    const services = db.SERVICES.find();
-    res.json(services);
-});
+        }
+    }
+];
 
-app.post('/register', async (req, res) => {
-    const info = req.body;
-    db.SERVICES.save(info);
-    const {data} = await axios.get(`${info.address}/test`);
-    console.log('test response', data);
-    return res.json({success: false});
-});
-
-io.on('connection', (socket) => {
-    console.log('new service connected', socket.id);
-    socket.emit('register', {id: socket.id});
-    socket.on('register', (data) => {
-        db.CONNECTIONS.save({
-            id: socket.id,
-            address: data.address
-        });
-    });
-    socket.on('disconnect', (reason) => {
-        console.log('socket disconnected', socket.id, reason);
-        db.CONNECTIONS.remove({id: socket.id});
-    });
-});
+ROUTES.forEach(e => app[e.method](e.route, e.action));
 
 http.listen(PORT, '0.0.0.0', () => {
     console.log(`Discovery Service Listening on ${PORT}`);
 });
 
-process.on('SIGINT', function() {
-    console.log("Shutting down server...");
-    db.CONNECTIONS.remove();
-    db.SERVICES.remove();
+io.on('connection', (socket) => {
+    socket.emit('register', {socketId: socket.id});
+    socket.on('disconnect', (reason) => {
+        console.log('service disconnected', socket.id, reason);
+        const service = db.SERVICES.findOne({socketId: socket.id});
+        db.SERVICES.remove({socketId: socket.id});
+        Object.keys(TYPES).forEach(e => {
+            TYPES[e].matches = TYPES[e].matches.filter(m => m.id !== service._id)
+        })
+    });
 });
 
-/*
-Client starts, connects to server via socket for 'alive' status
-use http req to register itself on the server, server tests connection and confirms addition
-server adds client to db of available services
-all routes are visible, as well as regex for things it can handle
- */
+process.on('SIGINT', function () {
+    console.log("Shutting down server...");
+    TABLES.forEach(e => db[e].remove());
+});
--- service/app.js
+++ service/app.js
@@ -1,52 +1,63 @@
 const PORT = 3000;
 const DISCOVERY_ADDRESS = 'http://localhost:8888';
-const app =  require('express')();
+const app = require('express')();
 const http = require('http').createServer(app);
 const socket = require('socket.io-client')(DISCOVERY_ADDRESS);
 const axios = require('axios');
 
+const METADATA = {
+    address: `http://localhost:${PORT}`,
+    name: 'Test Service',
+    description: 'This is a test description of the service!',
+    colour: '#ff0000',
+    matcher: [
+        {
+            type: 'IP_ADDRESS',
+            route: '/api/echo?q=%s'
+        }
+    ]
+};
+
 const ROUTES = [
     {
-        route: '/api/hello',
+        route: '/api/echo',
         method: 'get',
+        name: 'EchoParam',
+        description: 'echos the ip address sent to it.',
+        urlParams: ['q'],
+        hidden: false,
         action: (req, res) => {
-            console.log('saying hello');
-            return res.json({message: 'Hello!'});
+            const ip = req.query.q;
+            return res.json({ip});
         }
     }
 ];
 
-// super easy route making
+ROUTES.push({
+    route: '/test',
+    method: 'get',
+    name: 'Test',
+    description: 'Test route for the discovery service.',
+    hidden: true,
+    action: (req, res) => res.json({success: true})
+});
+
 ROUTES.forEach(e => app[e.method](e.route, e.action));
 
-socket.on('connect', () => {
-    console.log('connected to discovery');
-});
-
-socket.on('register', (data) => {
-    axios.post(`${DISCOVERY_ADDRESS}/register`, {
-        address: `http://localhost:${PORT}`,
-        name: 'Test',
-        description: 'This is a test description of the service!',
-        colour: '#ff0000',
-        routes: ROUTES.map(({method, route}) => ({method, route}))
-    }).then(({ data }) => {
-        console.log(data);
-    }).catch(console.error);
-   socket.emit('register', {
-       id: data.id,
-       address: `http://localhost:${PORT}`
-   });
-});
-
-socket.on('disconnect', () => {
-    console.log('disconnected from discovery');
-});
-
-app.get('/test', (req, res) => {
-    res.json({response: 200});
-});
-
 http.listen(PORT, '0.0.0.0', () => {
-    console.log(`Test Service listening on ${PORT}`);
+    console.log(`${METADATA.name} listening on ${PORT}`);
+});
+
+socket.on('register', async ({socketId}) => {
+    await axios.post(`${DISCOVERY_ADDRESS}/register`, {
+        ...METADATA,
+        socketId,
+        routes: ROUTES.filter(e => !e.hidden).map(({method, route, name, description, urlParams}) => ({
+            method,
+            route,
+            urlParams,
+            name,
+            description
+        }))
+    });
 });
